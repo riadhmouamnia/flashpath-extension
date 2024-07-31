ed6c93467f7ba43cf6ccd12b392957236581f187
Riadh Mouania <riadh.mouamnia@gmail.com>
2024-07-31 15:37:47 +0100

    chore: remove isolated events on youtube strategy

--- entrypoints/yt.content/index.tsx
+++ entrypoints/yt.content/index.tsx
@@ -14,7 +14,6 @@ export default defineContentScript({
       position: "inline",
       anchor: "body",
       append: "first",
-      isolateEvents: true,
       onMount: (container) => {
         // Don't mount react app directly on <body>
         const wrapper = document.createElement("div");
